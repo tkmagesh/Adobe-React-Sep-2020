3495d91397f0e1e1580efff5a77b6faa203234cc
Magesh Kuppan <tkmagesh77@gmail.com>
2020-09-08 14:40:07 +0530

    web server complete

--- 03-node/05-web-server/server.js
+++ 03-node/05-web-server/server.js
@@ -1,16 +1,28 @@
 const http = require('http'),
-    fs = require('fs');
+    fs = require('fs'),
+    path = require('path');
 
 const server = http.createServer(function(req /* IncomingMessage */, res /* ServerResponse */){
+    console.log(req.url);
     /* 
     res.write('<h1>Welcome to Node.js!</h1>');
     res.end(); 
     */
-    const stream = fs.createReadStream('./index.html');
+    //const stream = fs.createReadStream('./index.html');
     /* 
     stream.on('data', chunk => res.write(chunk));
     stream.on('end', () => res.end()); 
     */
+
+    var resourceName = req.url === '/' ? 'index.html' : req.url;
+    var resourceFullName = path.join(__dirname, resourceName);
+
+    if (!fs.existsSync(resourceFullName)) {
+        res.statusCode = 404;
+        res.end();
+        return;
+    }
+    const stream = fs.createReadStream(resourceFullName);
     stream.pipe(res);
 });
 
