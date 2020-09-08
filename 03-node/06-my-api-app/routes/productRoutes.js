fbf0fadf18bde0a07c61becad4ee97d85baec204
Magesh Kuppan <tkmagesh77@gmail.com>
2020-09-08 17:08:20 +0530

    Day-02 complete

--- 03-node/06-my-api-app/routes/productRoutes.js
+++ 03-node/06-my-api-app/routes/productRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express'),
     router = express.Router();
 
-const products = [
+let products = [
     { id: 101, name: 'Pen', cost: 10, units: 100 },
     { id: 102, name: 'Pencil', cost: 5, units: 500 },
     { id: 103, name: 'Marker', cost: 50, units: 50 },
@@ -20,5 +20,35 @@ router.get('/:id', function(req, res, next){
     }
     next();
 });
+
+router.post('/', function(req, res, next){
+    const newProductId = products.reduce((result, product) => result > product.id ? result : product.id, 0) + 1;
+    const newProduct = { ...req.body, id: newProductId };
+    products.push(newProduct);
+    res.status(201).json(newProduct);
+});
+
+router.put('/:id', function(req, res, next){
+    const pid = parseInt(req.params.id),
+        existingProduct = products.find(p => p.id === pid);
+    if (existingProduct) {
+        const updatedProduct = req.body;
+        products = products.map(product => product.id === pid ? updatedProduct : product);
+        return res.json(updatedProduct);
+    }
+    next();
+});
+
+router.delete('/:id', function (req, res, next) {
+    const pid = parseInt(req.params.id),
+        existingProduct = products.find(p => p.id === pid);
+    if (existingProduct) {
+        products = products.filter(product => product.id !== pid);
+        return res.json({});
+    }
+    next();
+});
+
+
 
 module.exports = router;
