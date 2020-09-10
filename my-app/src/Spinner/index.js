4dcfe2499b93a0cfa8381a119bdcd706fef01bb9
Magesh Kuppan <tkmagesh77@gmail.com>
2020-09-10 16:28:29 +0530

    Spinner as a functional component

--- my-app/src/Spinner/index.js
+++ my-app/src/Spinner/index.js
@@ -1,41 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class Spinner extends Component {
-  state = {
-    delta: 0
-  };
-
-  onUpClick = () => {
-    const { delta } = this.state;
-    const up_action = this.props.up(delta);
-    this.props.dispatch(up_action);
-  };
-  onDownClick = () => {
-    const { delta } = this.state;
-    const down_action = this.props.down(delta);
-    this.props.dispatch(down_action);
-  };
-  render() {
-    const { value } = this.props;
-    return (
-      <div>
-        <h3>Spinnner</h3>
-        <hr />
-        <label>Delta : </label>
-        <input
-          type="number"
-          value={this.state.delta}
-          onChange={evt =>
-            this.setState({ delta: parseInt(evt.target.value) || 0 })
-          }
-        />
-        <br />
-        <input type="button" value="Down" onClick={this.onDownClick} />
-        <span> [ {value} ] </span>
-        <input type="button" value="Up" onClick={this.onUpClick} />
-      </div>
-    );
-  }
+const Spinner = ({ value, dispatch, up, down }) => {
+  const [delta, setDelta] = useState(0);
+  return (
+    <div>
+      <h3>Spinnner</h3>
+      <hr />
+      <label>Delta : </label>
+      <input
+        type="number"
+        value={delta}
+        onChange={evt => setDelta(parseInt(evt.target.value) || 0)}
+      />
+      <br />
+      <input type="button" value="Down" onClick={() => dispatch(down(delta))} />
+      <span> [ {value} ] </span>
+      <input type="button" value="Up" onClick={() => dispatch(up(delta))} />
+    </div>
+  );
 }
 
 export default Spinner;
