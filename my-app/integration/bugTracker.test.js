4dd791e5b6ebe996901ec202e47acb637dfffc1a
Magesh Kuppan <tkmagesh77@gmail.com>
2020-09-12 18:13:00 +0530

    updated with the working version of the test

--- my-app/integration/bugTracker.test.js
+++ my-app/integration/bugTracker.test.js
@@ -5,7 +5,32 @@ describe("bugTracker", () => {
   });
 
   it('Should toggle the bug when clicked', async () => {
-    await page.click("span.bugname");
+
+    //wait for the page to load with bugnames
+    await page.waitForSelector("span.bugname");
+
+    //get all the span with the class 'bugname
+    const bugNameSpans = await page.$$('span.bugname');
+
+    //find out the first span with text "Server communication failure"
+    const bugNameSpan = bugNameSpans.find(
+      async element =>
+        await element.evaluate(node => node.innerText === "Search communication failure")
+    );
+
+    //get if the span has the class "closed"
+    const toggledFlag = await bugNameSpan.evaluate(node => node.classList.contains('closed'));
+
+    //click the span
+    await bugNameSpan.click();
+
+    //get if the span has the class "closed"
+    const updatedToggledFlag = await bugNameSpan.evaluate(node =>
+      node.classList.contains("closed")
+    );
+
+    //ensure the "closed" class is toggled
+    expect(toggledFlag).toBe(!updatedToggledFlag);
 
   })
 });
